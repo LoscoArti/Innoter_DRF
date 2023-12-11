ab147481121cdf471054c762989beb2591df28f9
arti <loscoarti@gmail.com>
2023-12-11 11:53:45 +0100

    Add .gitignore and update database settings
    Add rest_framework and posts app
    Define models
    
    Seperate by apps
    Models implementation
    
    Register multiple models in admin.py files

---
+++ backend/posts/models.py
@@ -0,0 +1,21 @@
+import uuid
+from django.db import models
+
+from pages.models import Page
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    page = models.ForeignKey(Page, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    reply_to = models.ForeignKey(
+        "self", null=True, blank=True, on_delete=models.SET_NULL, related_name="replies"
+    )
+    likes_user_ids = models.JSONField(default=list)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_likes")
+    user_id = models.UUIDField()
